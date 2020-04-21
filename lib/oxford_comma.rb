65d26e61ef69a6d4f75b280e4b72364136f1b9f2
Joseph Romero <joeromero47@gmail.com>
2020-04-21 18:00:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,15 @@
 def oxford_comma(array)
-  array = ["kiwi"].join
-  return array
-  array << ["durian"]
-  return array
+   array = [*array]
+  case array.size
+  when 0
+    ''
+  when 1
+    array[0].to_s
+  when 2
+    array.join(' and ')
+  else
+    array_copy = array
+    array_copy[-1] = "and #{array_copy[-1]}"
+    array_copy.join(', ')
+  end
 end
