fca1be5c631853f72a21cc15f0d4e2dd329977c2
Joseph Romero <joeromero47@gmail.com>
2020-04-21 18:28:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     when 1
       array.first
     when 2
-      array.join('and')
+      array.join(' and ')
     else
       [array[0..-2].join(','), array.last(', and')]
     end
